c056eabe4c35fd0eb8fd2d2563a49317ec468fc1
bmuhammad <briancmuhammad@yahoo.com>
2025-03-15 17:31:26 -0700

    Fix error of not strictly setting response type by adding extension method of defaultrequestheader.accept and adding application/json as response type
    added code to accept both json and xml reponse types by adding code to deserialize both types

--- Movies.Client/Services/CRUDSamples.cs
+++ Movies.Client/Services/CRUDSamples.cs
@@ -25,16 +25,37 @@ public class CRUDSamples : IIntegrationService
         httpClient.BaseAddress = new Uri("http://localhost:5001");
         httpClient.Timeout = new TimeSpan(0, 0, 30);
 
+        httpClient.DefaultRequestHeaders.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.DefaultRequestHeaders.Accept.Add(
+           new MediaTypeWithQualityHeaderValue("application/xml"));
+
+
         var response = await httpClient.GetAsync("api/movies");
         response.EnsureSuccessStatusCode();
 
         var content = await response.Content.ReadAsStringAsync(); ;
-        var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(
-            content,
-            new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+
+        var movies = new List<Movie>();
+
+
+        if (response.Content.Headers.ContentType?.MediaType
+           == "application/json")
+        {
+            movies = JsonSerializer.Deserialize<List<Movie>>(
+                content,
+                new JsonSerializerOptions
+                { 
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+        }
+        else if (response.Content.Headers.ContentType?.MediaType
+            == "application/xml")
+        {
+            var serializer = new XmlSerializer(typeof(List<Movie>));
+            movies = serializer.Deserialize(
+                new StringReader(content)) as List<Movie>;
+        }
 
     }
 
