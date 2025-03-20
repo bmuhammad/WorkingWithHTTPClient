3eaec4f06e328fc817962fb693cc7f10100d80e7
bmuhammad <briancmuhammad@yahoo.com>
2025-03-20 15:38:33 -0700

    Added method for disposing httpclient instance. This method creates too many open connections.

--- Movies.Client/Services/HttpClientFactorySamples.cs
+++ Movies.Client/Services/HttpClientFactorySamples.cs
@@ -2,8 +2,28 @@
 
 public class HttpClientFactorySamples : IIntegrationService
 {
-    public Task RunAsync()
+    public async Task RunAsync()
     {
-        throw new NotImplementedException();
+         await TestDisposeHttpClientAsync();
+    }
+
+    private async Task TestDisposeHttpClientAsync()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage(
+                    HttpMethod.Get,
+                    "https://www.google.com");
+
+                var response = await httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Request completed with status code " +
+                    $"{response.StatusCode}");
+            }
+        }
     }
 }
