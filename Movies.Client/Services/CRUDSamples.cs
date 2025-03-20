165572ac85a3ce7b69ea73a8d2c80b32bdbcb1f2
bmuhammad <briancmuhammad@yahoo.com>
2025-03-20 12:09:18 -0700

    Added shortcuts for creating a resorce (post), updating a resource (put) and deleting a resource. Shortcuts dont send header content by default.  Short cuts may not be best to use if content header info needed would  be different each call or response

--- Movies.Client/Services/CRUDSamples.cs
+++ Movies.Client/Services/CRUDSamples.cs
@@ -1,6 +1,7 @@
 ﻿using Movies.Client.Helpers;
 using Movies.Client.Models;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Xml.Serialization;
 
@@ -171,6 +172,77 @@ public class CRUDSamples : IIntegrationService
             new MediaTypeWithQualityHeaderValue("application/json"));
 
         var response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+    }
+
+    private async Task PostResourceShortcutAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var movieToCreate = new MovieForCreation()
+        {
+            Title = "Reservoir Dogs",
+            Description = "After a simple jewelry heist goes terribly wrong, the " +
+            "surviving criminals begin to suspect that one of them is a police informant.",
+            DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+            ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+            Genre = "Crime, Drama"
+        };
+
+        var response = await httpClient.PostAsync(
+            "api/movies",
+            new StringContent(
+                JsonSerializer.Serialize(movieToCreate,
+                _jsonSerializerOptionsWrapper.Options),
+                Encoding.UTF8,
+                "application/json"));
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var createdMovie = JsonSerializer.Deserialize<Movie>(
+            content,
+            _jsonSerializerOptionsWrapper.Options);
+    }
+
+    private async Task PutResourceShortcut()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var movieToUpdate = new MovieForUpdate()
+        {
+            Title = "Pulp Fiction",
+            Description = "The movie with Zed.",
+            DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+            ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+            Genre = "Crime, Drama"
+        };
+
+        var response = await httpClient.PutAsync(
+           "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b",
+           new StringContent(
+               JsonSerializer.Serialize(movieToUpdate,
+                _jsonSerializerOptionsWrapper.Options),
+               System.Text.Encoding.UTF8,
+               "application/json"));
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var updatedMovie = JsonSerializer.Deserialize<Movie>(
+            content,
+            _jsonSerializerOptionsWrapper.Options);
+    }
+
+
+    private async Task DeleteResourceShortcut()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var response = await httpClient.DeleteAsync(
+            "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b");
         response.EnsureSuccessStatusCode();
 
         var content = await response.Content.ReadAsStringAsync();
