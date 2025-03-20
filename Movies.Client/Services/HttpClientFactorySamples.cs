48e0c4063acf5e24f91d2487c7e2bded63918cc2
bmuhammad <briancmuhammad@yahoo.com>
2025-03-20 16:05:23 -0700

    added code that configured httpClients

--- Movies.Client/Services/HttpClientFactorySamples.cs
+++ Movies.Client/Services/HttpClientFactorySamples.cs
@@ -1,11 +1,55 @@
-﻿namespace Movies.Client.Services;
+﻿using Movies.Client.Helpers;
+using Movies.Client.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Movies.Client.Services;
 
 public class HttpClientFactorySamples : IIntegrationService
 {
+
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly JsonSerializerOptionsWrapper _jsonSerializerOptionsWrapper;
+   /// private readonly MoviesAPIClient _moviesAPIClient;
+
+    public HttpClientFactorySamples(IHttpClientFactory httpClientFactory,
+         JsonSerializerOptionsWrapper jsonSerializerOptionsWrapper)
+    {
+        _jsonSerializerOptionsWrapper = jsonSerializerOptionsWrapper ??
+            throw new ArgumentNullException(nameof(jsonSerializerOptionsWrapper));
+      //  _moviesAPIClient = moviesAPIClient ??
+        //    throw new ArgumentNullException(nameof(moviesAPIClient));
+        _httpClientFactory = httpClientFactory ??
+            throw new ArgumentNullException(nameof(httpClientFactory));
+    }
+
     public async Task RunAsync()
     {
-        // await TestDisposeHttpClientAsync();
-         await TestReuseHttpClientAsync();
+        // await
+        // TestDisposeHttpClientAsync();
+       //  await TestReuseHttpClientAsync();
+         await GetFilmsAsync();
+    }
+
+    public async Task GetFilmsAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            "api/films");
+        request.Headers.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(
+            content,
+            _jsonSerializerOptionsWrapper.Options);
     }
 
     private async Task TestDisposeHttpClientAsync()
