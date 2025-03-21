e89f42dff91780c3464f73716bb18c2d3777f2e4
bmuhammad <briancmuhammad@yahoo.com>
2025-03-21 14:28:06 -0700

    Added paramenter to method in order to improve memory and performance. Used HttpCompletionOption class with ResponseHeadersRead property. Will demonstrate improvement in next checkin

--- Movies.Client/Services/LocalStreamsSamples.cs
+++ Movies.Client/Services/LocalStreamsSamples.cs
@@ -21,7 +21,8 @@ public class LocalStreamsSamples : IIntegrationService
     }
     public async Task RunAsync()
     {
-         await GetPosterWithStreamAsync();
+        //await GetPosterWithStreamAsync();
+        await GetPosterWithStreamAndCompletionModeAsync();
     }
 
     private async Task GetPosterWithStreamAsync()
@@ -38,7 +39,29 @@ public class LocalStreamsSamples : IIntegrationService
         {
             response.EnsureSuccessStatusCode();
 
-            var stream = await response.Content.ReadAsStreamAsync();
+            var stream = await response.Content.ReadAsStreamAsync(); //needs to be disposed. using is used
+            var poster = await JsonSerializer.DeserializeAsync<Poster>(
+                    stream,
+                    _jsonSerializerOptionsWrapper.Options);
+        }
+    }
+
+    private async Task GetPosterWithStreamAndCompletionModeAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+        request.Headers.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        using (var response = await httpClient.SendAsync(request,
+           HttpCompletionOption.ResponseHeadersRead))
+        {
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync(); //needs to be disposed. using is used
             var poster = await JsonSerializer.DeserializeAsync<Poster>(
                     stream,
                     _jsonSerializerOptionsWrapper.Options);
