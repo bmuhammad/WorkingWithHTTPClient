de8bf9a652def8635eb746c199429683c5e47bf4
bmuhammad <briancmuhammad@yahoo.com>
2025-03-22 08:46:44 -0700

    Added new project for testing methods. Used xunit added code for unit testing with separate classes and code used test explore in visual studio to unit test without calling actual api

---
+++ Movies.Client/Handlers/Return401UnauthorizedREsponsehandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Handlers
+{
+    public class Return401UnauthorizedResponseHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(
+                        System.Net.HttpStatusCode.Unauthorized);
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ Movies.Client/UnauthorizedApiAccessException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Client
+{
+    [Serializable]
+    public class UnauthorizedApiAccessException : Exception
+    {
+        public UnauthorizedApiAccessException()
+        {
+        }
+
+        public UnauthorizedApiAccessException(string? message) : base(message)
+        {
+        }
+
+        public UnauthorizedApiAccessException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnauthorizedApiAccessException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
